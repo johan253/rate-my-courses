7132d949e7b58c5b73020f8da6f9c82577e44d21
johan253 <johannjo2000@gmail.com>
2024-03-07 13:04:31 -0800

    Set up course card component

---
+++ src/components/CourseCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function CourseCard(props) {
+    return (
+        <div>
+            <h3>
+                {props.name}
+            </h3>
+            <h4>
+                {}
+            </h4>
+        </div>
+    );
+}
+
+export default CourseCard;
