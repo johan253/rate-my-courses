b8f12c52051378253e7e31465cb75f9601e2519a
johan253 <johannjo2000@gmail.com>
2024-06-26 14:44:41 -0700

    Added searchCourse cloud function for future use, and deployed it

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,6 +17,12 @@ import * as admin from "firebase-admin";
 const logger = functions.logger;
 admin.initializeApp();
 
+/**
+ * Hello World function
+ *
+ * @param {Request} request - The request object.
+ * @param {Response} response - The response object.
+ */
 export const helloWorld = functions.https.onRequest(
   (request: Request, response: Response) => {
     if (request.method === "GET") {
@@ -26,7 +32,12 @@ export const helloWorld = functions.https.onRequest(
       response.status(405).send("Method Not Allowed");
     }
   });
-
+/**
+ * Echo the message sent in the request body
+ *
+ * @param {Request} request - The request object.
+ * @param {Response} response - The response object.
+ */
 export const echo = functions.https.onRequest(
   (request: Request, response: Response) => {
     if (request.method === "POST") {
@@ -36,7 +47,12 @@ export const echo = functions.https.onRequest(
       response.status(405).send("Method Not Allowed");
     }
   });
-
+/**
+ * Get a school by its ID
+ *
+ * @param {Request} request - The request object.
+ * @param {Response} response - The response object.
+ */
 export const getSchool = functions.https.onRequest(
   async (request: Request, response: Response) => {
     if (request.method === "GET") {
@@ -60,17 +76,22 @@ export const getSchool = functions.https.onRequest(
           }
         } else {
           response.status(404).send({error: "School not found",
-            request: request.query});
+            request: request.body});
         }
       } else {
         response.status(400).send({error: "schoolId not provided",
-          request: request.query});
+          request: request.body});
       }
     } else {
       response.status(405).send("Method Not Allowed");
     }
   });
-
+/**
+ * Get a course by its ID
+ *
+ * @param {Request} request - The request object.
+ * @param {Response} response - The response object.
+ */
 export const getCourse = functions.https.onRequest(
   async (request: Request, response: Response) => {
     if (request.method === "GET") {
@@ -81,29 +102,78 @@ export const getCourse = functions.https.onRequest(
         if (course.exists) {
           const data = course.data();
           if (data) {
-            const school = admin
+            const school = await admin
               .firestore()
               .collection("schools")
-              .doc(data.school.toString())
+              .doc(data.school.split("/").pop())
               .get();
             response.send({
               id: course.id,
               name: data.name,
               ratings: data.ratings,
-              school: school,
+              school: school.data(),
             });
           } else {
+            logger.error(`Data not available for course: ${courseId}`);
             response.status(500).send({error: "Data not available"});
           }
         } else {
+          logger.warn(`Course not found: ${courseId}`);
           response.status(404).send({error: "Course not found",
             request: request.query});
         }
       } else {
+        logger.error("courseId not provided");
         response.status(400).send({error: "courseId not provided",
           request: request.query});
       }
     } else {
+      logger.error("Method Not Allowed, attempt to use ", request.method);
       response.status(405).send("Method Not Allowed");
+    }
+  });
+/**
+ * Search for courses by name
+ *
+ * @param {Request} request - The request object.
+ * @param {Response} response - The response object.
+ */
+export const searchCourses = functions.https.onRequest(
+  async (request: Request, response: Response) => {
+    // Only allow GET requests
+    if (request.method !== "GET") {
+      response.status(405).send("Method Not Allowed");
+      return;
+    }
+    // Prevent empty query
+    const query: string = request.body.query;
+    if (!query || query.trim() === "") {
+      response.status(400).send({error: "query not provided | empty",
+        request: request.body});
+      return;
+    }
+    try {
+      // Query Firestore for courses
+      const snapshot = await admin.firestore()
+        .collection("courses")
+        .where("name", ">=", query.toUpperCase())
+        .where("name", "<=", query.toUpperCase() + "\uf8ff")
+        .get();
+
+      if (snapshot.empty) {
+        response.status(404).send({error: "No courses found",
+          request: request.body});
+        return;
+      }
+      // Return courses
+      const courses: admin.firestore.DocumentData = [];
+      snapshot.forEach((doc) => {
+        courses.push({id: doc.id, ...doc.data()});
+      });
+
+      response.status(200).send(courses);
+    } catch (error) {
+      logger.error("Error getting documents", error);
+      response.status(500).send({error: "Error getting documents"});
     }
   });
