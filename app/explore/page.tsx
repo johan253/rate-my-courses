9deac95e2b5bd4c11d01ef3e19cff37bba36fcc0
johan253 <johannjo2000@gmail.com>
2024-11-04 18:53:31 -0800

    Added message if no trending courses

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -10,10 +10,12 @@ export default async function ExplorePage() {
   return (
     <main className="p-12">
       <h1 className="text-3xl font-bold mb-6 text-center">
-        Trending Courses in the last 24 hours
+        Top 5 trending courses in the last 24 hours
       </h1>
       <ul>
-        {courseCards}
+        {courseCards.length !== 0 ?
+          (courseCards) : (<p className="text-center p-4">No trending courses...</p>)
+        }
       </ul>
       <p className="italic text-gray-500 text-center p-4">
         Trending courses are based on the number and ratio of good reviews
