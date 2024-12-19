6349291f2c0802da73c770c7135f65914f7b69f9
johan253 <johannjo2000@gmail.com>
2024-12-18 16:28:38 -0800

    Implemented trending courses query using Kysely

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,10 +1,38 @@
-import { getTrendingCourses } from "@/lib/actions";
+import { db } from "@/lib/kysely";
+import { sql } from "kysely";
+
 import CourseCard from "@/components/CourseCard";
 
 export default async function ExplorePage() {
-  const courses = await getTrendingCourses();
-  const courseCards = courses.map((course) => (
-    <CourseCard key={course.id} course={course} school={course.school} />
+
+  const trendingCourses = await db
+    .selectFrom("Course")
+    .innerJoin("School", "Course.schoolId", "School.id")
+    .innerJoin("Rating", "Course.id", "Rating.courseId")
+    .select([
+      "Course.id",
+      "Course.code",
+      "Course.updatedAt",
+      "Course.createdAt",
+      "Course.schoolId",
+      "Course.authorId",
+      sql`
+        json_build_object('id', "School".id, 'name', "School"."name", 'location', "School"."location")
+      `.as("school"),
+      sql`
+        avg("Rating".rating)
+      `.as("rating_ratio"),
+    ])
+    .where("Rating.createdAt", ">", new Date(Date.now() - 24 * 60 * 60 * 1000))
+    .orderBy("rating_ratio", "desc")
+    .orderBy(sql`count("Rating".id)`, "desc")
+    .limit(5)
+    .groupBy("Course.id")
+    .groupBy("School.id")
+    .execute();
+
+  const courseCards = trendingCourses.map((course) => (
+    <CourseCard key={course.id} course={course} school={course.school as any} />
   ));
 
   return (
