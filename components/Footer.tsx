1ad5f56f29818de7b10b3a4dbc53411cf3884269
johan253 <johannjo2000@gmail.com>
2025-02-11 21:20:53 -0800

    Testing automatic docker image updates from Dockerhub

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="bg-gray-100 p-4 text-center flex flex-col items-center">
       <ul className="flex gap-2 justify-around w-screen">
         <li><Link href="/">Help</Link></li>
-        <li><Link href="mailto:johan.hdzz@outlook.com">Contact</Link></li>
+        <li><Link href="mailto:johannjo2000@gmail.com">Contact</Link></li>
         <li><Link href="/">About</Link></li>
         <li><Link href="/">Guidelines</Link></li>
         <li><Link href="/">Terms & Conditions</Link></li>
